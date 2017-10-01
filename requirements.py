61a27a66c74736823eb0edba4d887d111456d4af
Guinsly Mondésir <guinslym@gmail.com>
2017-09-30 21:26:10 -0400

    adding index.html

--- requirements.py
+++ requirements.py
@@ -1,1 +1,5 @@
 requests==2.18.4
+beautifulsoup4==4.6.0
+lxml==4.0.0
+html5lib
+html5lib==0.999999999
---
+++ scrap.py
@@ -0,0 +1,8 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+with open("index.html") as fp:
+    soup = BeautifulSoup(fp)
+
+soup = BeautifulSoup("<html>data</html>")
